e6a13cfc6a8f855e21b50b8f6f496261b48b5d61
j-aiha <comp.bru@gmail.com.br>
2020-05-27 15:48:38 -0300

    pagina nova, perfil user n terminada

---
+++ profile.php
@@ -0,0 +1,70 @@
+<html>
+
+<head>
+    <link rel="shortcut icon" href="img/favicon.ico" />
+    <link rel="stylesheet" type="text/css" href="css/slick.scss" />
+    <link rel="stylesheet" href="icofont/icofont.min.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <link rel="stylesheet" href="bootstrap/bootstrap.min.css">
+    <title>perfil</title>
+</head>
+
+<body>
+    <header class="menu-principal">
+        <main>
+            <div class="header-1">
+                <div class="logo">
+                    <a title="Raruna!" href="index.php"><img src="./img/Logo.png"></a>
+                </div>
+                <div class="interacoes">
+                    <a title="Logout" href="register.php"><button class="btn btn-dark btn-sm">Logout</button></a>
+
+                </div>
+            </div>
+        </main>
+    </header>
+    <main class="coluna-100 menu-urls">
+        <div class="header-2">
+            <div class="menu">
+                <ul>
+                    <li><a title="Home" href="index.php">Home</a></li>
+                    <li><a title="Profile" href="profile.php">Profile</a></li>
+                    <li><a href="">Musicas</a></li>
+                    <li><a href="">Feed</a></li>
+                </ul>
+
+            </div>
+            <div class="busca">
+                <input placeholder="Search Something" type="text">
+            </div>
+        </div>
+    </main>
+    <!--<div class="imgUser">
+        <img src="./img/hisoka.jpg" class="mr-3" alt="...">
+        <div class="media-body">
+
+        </div>
+    </div>-->
+
+    <div class="box">
+        <label class="avatar" for="btn">
+            <img src="./img/hisoka.jpg" alt="">
+        </label>
+        <input type="checkbox" id="btn">
+        <div class="pichange">
+            <a href="#"><i class="fa fa-upload"> <span>upload</span></i></a>
+            <br>
+            <a href="#"><i class="fa fa-edit"> <span>edite</span></i></a>
+        </div>
+    </div>
+
+    <footer>
+        <script type="text/javascript" src="js/jquery.js"></script>
+        <script type="text/javascript" src="js/jquery-migrate.js"></script>
+        <script type="text/javascript" src="js/slick.min.js"></script>
+        <script type="text/javascript" src="./js/main.js"></script>
+        <script src="bootstrap/bootstrap.min.js"></script>
+    </footer>
+</body>
+
+</html>
