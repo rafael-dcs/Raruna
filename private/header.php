357f7f5a4adbff02e2a06815cbfb23d3cf6ece60
j-aiha <comp.bru@gmail.com.br>
2020-05-27 21:28:28 -0300

    mais alteracao

---
+++ private/header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="img/favicon.ico" />
+    <link rel="stylesheet" type="text/css" href="css/slick.scss" />
+    <link rel="stylesheet" href="icofont/icofont.min.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <link rel="stylesheet" href="bootstrap/bootstrap.min.css">
+    <title>perfil</title>
+</head>
+
+<body>
+    <header class="menu-principal">
+
+        <div class="header-1">
+            <div class="logo">
+                <a title="Raruna!" href="index.php"><img src="./img/Logo.png"></a>
+            </div>
+            <div class="interacoes">
+                <a title="Logout" href="register.php"><button class="btn btn-dark btn-sm">Logout</button></a>
+
+            </div>
+        </div>
+        <nav class="coluna-100 menu-urls">
+            <div class="header-2">
+                <div class="menu">
+                    <ul>
+                        <li><a title="Home" href="index.php">Home</a></li>
+                        <li><a title="Profile" href="profile.php">Profile</a></li>
+                        <li><a href="">Musicas</a></li>
+                        <li><a href="">Feed</a></li>
+                    </ul>
+
+                </div>
+                <div class="busca">
+                    <input placeholder="Search Something" type="text">
+                </div>
+            </div>
+        </nav>
+
+    </header>
---
+++ private/profile.php
@@ -0,0 +1,32 @@
+<?php
+include "header.php";
+?>
+
+<!--<div class="imgUser">
+        <img src="./img/hisoka.jpg" class="mr-3" alt="...">
+        <div class="media-body">
+
+        </div>
+    </div>-->
+
+<div class="box">
+    <label class="avatar" for="btn">
+        <img src="./img/hisoka.jpg" alt="">
+    </label>
+    <input type="checkbox" id="btn">
+    <div class="pichange">
+        <a href="#"><i class="fa fa-upload"> <span>upload</span></i></a>
+        <br>
+        <a href="#"><i class="fa fa-edit"> <span>edite</span></i></a>
+    </div>
+</div>
+
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/jquery-migrate.js"></script>
+<script type="text/javascript" src="js/slick.min.js"></script>
+<script type="text/javascript" src="./js/main.js"></script>
+<script src="bootstrap/bootstrap.min.js"></script>
+
+</body>
+
+</html>
