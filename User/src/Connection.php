5868f14e2c3f5011ac559edebfada0528ff9a809
rafael-dcs <rafael.dcs@unijui.edu.br>
2020-06-03 14:47:48 -0300

    reorganização (kkk)

---
+++ User/src/Connection.php
@@ -0,0 +1,12 @@
+﻿<?php
+
+$host="localhost";
+$port=3306;
+$user="root";
+$password="";
+$dbname="raruna";
+
+$con = new mysqli($host, $user, $password, $dbname, $port)
+	or die ('Could not connect to the database server' . mysqli_connect_error());
+
+//$con->close();
