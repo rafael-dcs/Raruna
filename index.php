5c77ea3e3bbd0e409a387be048df6163ea98c3de
j-aiha <comp.bru@gmail.com.br>
2020-06-03 19:31:15 -0300

    pull

--- index.php
+++ index.php
@@ -74,13 +74,13 @@ include "header.php";
         </div>
     </div>
 </div>
-<footer>
-    <script type="text/javascript" src="./assets/js/jquery.js"></script>
-    <script type="text/javascript" src="./assets/js/jquery-migrate.js"></script>
-    <script type="text/javascript" src="./assets/js/slick.min.js"></script>
-    <script type="text/javascript" src="./assets/js/main.js"></script>
-    <script src="assets/bootstrap/bootstrap.min.js"></script>
-</footer>
+
+<script type="text/javascript" src="assets/js/jquery.js"></script>
+<script type="text/javascript" src="assets/js/jquery-migrate.js"></script>
+<script type="text/javascript" src="assets/js/slick.min.js"></script>
+<script type="text/javascript" src="assets/js/main.js"></script>
+<script src="assets/bootstrap/bootstrap.min.js"></script>
+
 </body>
 
 </html>
