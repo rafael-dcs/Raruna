c8005c1bd664f4ba28936ae22ecbfa677d033ba6
j-aiha <comp.bru@gmail.com.br>
2020-06-06 17:58:36 -0300

    sla

--- header.php
+++ header.php
@@ -15,7 +15,21 @@
 <body>
     <header class="menu-principal">
 
-        <div class="header-1">
+        <div class="container-fluid justify-content-between">
+            <div class="row">
+                <div class="col-4 p-3">
+                    <a title="Raruna!" href="index.php"><img src="assets/img/Logo.png"></a>
+
+                </div>
+
+                <div class="col-4 p-3">
+                    <a title="Register" href="register.php"><button class="btn btn-primary btn-sm">Register</button></a>
+                    <a title="Login" href="login.php"><button class="btn btn-dark btn-sm">Login</button></a>
+                </div>
+
+            </div>
+
+            <!--<div class="header-1">
             <div class="logo">
                 <a title="Raruna!" href="index.php"><img src="assets/img/Logo.png"></a>
             </div>
@@ -23,19 +37,19 @@
                 <a title="Register" href="register.php"><button class="btn btn-primary btn-sm">Register</button></a>
                 <a title="Login" href="login.php"><button class="btn btn-dark btn-sm">Login</button></a>
             </div>
-        </div>
-        <nav class="coluna-100 menu-urls">
-            <div class="header-2">
-                <div class="menu">
-                    <ul>
-                        <li><a title="Home" href="index.php">Home</a></li>
-                        <li><a href="">Premium</a></li>
-                    </ul>
+        </div> -->
+            <nav class="coluna-100 menu-urls">
+                <div class="header-2">
+                    <div class="menu">
+                        <ul>
+                            <li><a title="Home" href="index.php">Home</a></li>
+                            <li><a href="">Premium</a></li>
+                        </ul>
 
+                    </div>
+                    <div class="busca">
+                        <input placeholder="Search Something" type="text">
+                    </div>
                 </div>
-                <div class="busca">
-                    <input placeholder="Search Something" type="text">
-                </div>
-            </div>
-        </nav>
+            </nav>
     </header>
